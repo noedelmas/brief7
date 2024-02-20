52ac3660202cb93e1e7b51fd9920dd2d954a3b7f
noedmw <noe.dwwm48@gmail.com>
2024-02-20 09:42:18 +0100

    test.php
    09h42 20/2/24

---
+++ footr2.php
@@ -0,0 +1,51 @@
+<?php
+    include 'index.php';
+?>
+
+<!-- FOOTR -->
+
+    <footer class="footer_alt">
+        <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <span class="col-md-3 col-12 text-center">
+                <h4>qui sommes-nous ?</h1>
+                <ul>
+                    <li><a href="https://www.greta-gard.com/qui-sommes-nous/" target="_blank">Présentation</a></li>
+                    <li><a href="https://www.greta-gard.com/publications/" target="_blank">Actualités</a></li>
+                    <li>Nos réseaux sociaux :</li>
+                    <span class="d-flex justify-content-center">
+                        <li class = "me-1"><a href="https://twitter.com/DRAFPICA_Occ" target="_blank"><i class="fa-brands fa-x-twitter x_twitter"></i></a></li>
+                        <li class = "ms-1"><a href="https://fr.linkedin.com/school/greta-occitanie/" target="_blank"><i class="linked_in fa-brands fa-linkedin"></i></a></li>
+                    </span>
+                </ul>
+            </span>
+            <span class="col-md-3 col-12 text-center">
+                <h4>infos pratiques</h4>
+                <ul>
+                    <li><a href="https://www.greta-gard.com/nos-agences/" target="_blank">Ou nous retrouver</a></li>
+                    <li><a href="https://www.greta-gard.com/apprentie/#les-aides" target="_blank">Financer votre formation</a></li>
+                    <li><a href="https://www.greta-gard.com/se-former-avec-un-handicap/" target="_blank">Se former en <br>
+                    situation d'handicap</a></li>
+                </ul>
+            </span>
+            <span class="col-md-3 col-12 text-center">
+                <h4>les formations</h4>
+                <ul>
+                    <li><a href="https://www.greta-gard.com/formations/" target="_blank">Catalogue des formations</a></li>
+                    <li><a href="https://www.greta-gard.com/liste-des-formations/digital-informatique/" target="_blank">CFA du <br>numérique</a></li>
+                    <li><a href="https://www.greta-gard.com/inscriptions/#info-collective" target="_blank">Agenda des formations</a></li>
+                </ul>
+            </span>
+            
+        </div>
+        <div class="col-md-4 offset-md-4 footr_alt mt-3">
+            <span class=" text-center">
+                <ul>
+                    <h2 class="mt-4 pt-2 text-center ">Design by bretzeldesign ft.wordpress</h2>
+                    
+                </ul>
+            </span>
+            <p class="text-center pb-2">© 2024 GRETA, Tous droits réservés | Mentions légales</p>
+        </div>
+    </footer>
+
+<!-- FOOTR -->
